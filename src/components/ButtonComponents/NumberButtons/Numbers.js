f38593134002a199a40de08fd0db661ea63e5822
Yara Castro <yaracastro24@gmail.com>
2019-07-30 20:21:33 -0500

    Operator button to work

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 import NumberButton from "./NumberBytton.js";
 //Import your array data to from the provided data file
-import data from "./scr/Img/data.js";
-
+import data from "./../../data.js";
 const numbers = data.numbers;
 
 const Numbers = () => {
+  const [numberState, setNumberState] = useState(numbers);
   // STEP 2 - add the imported data to state
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
       component matching the name on the provided file. Pass
        it any props needed by the child component */}
-    {numbers.map(function(num){ return <NumberButton numbers={numbers[i]/>})}
+      {numbers.map(numberObj =>{
+        return <NumberButton number={numberObj}/>;
+      })
+      }
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
 const OperatorButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{tsPropertySignature.operators}</button>
     </>
   );
 };
