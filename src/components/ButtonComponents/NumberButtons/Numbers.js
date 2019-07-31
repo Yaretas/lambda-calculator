a9733341dc0ee390755b040daecc4acd4006d0b8
Yara Castro <yaracastro24@gmail.com>
2019-07-30 20:04:14 -0500

    initial commit

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 //import any components needed
-
+import NumberButton from "./NumberBytton.js";
 //Import your array data to from the provided data file
+import data from "./scr/Img/data.js";
+
+const numbers = data.numbers;
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      component matching the name on the provided file. Pass
+       it any props needed by the child component */}
+    {numbers.map(function(num){ return <NumberButton numbers={numbers[i]/>})}
     </div>
   );
 };
